f5e115c462b7865d2a271a4abf998822c77f5f79
Smart developer <developer@yandex.ru>
2025-04-11 21:55:52 +1000

    Mars

--- flask__/_sqlalchemy/main.py
+++ flask__/_sqlalchemy/main.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
-
-import datetime
+from forms.user import RegisterForm
 
 db_session.global_init("db/mars_explorer.db")
 db_sess = db_session.create_session()
@@ -13,11 +12,39 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-
 @app.route("/")
 def index():
     jobs = db_sess.query(Jobs).all()
     return render_template("works_log.html", works=jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Registration',
+                                   form=form,
+                                   message="Passwords don't match")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Registration',
+                                   form=form,
+                                   message="This email is already registered")
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            address=form.address.data,
+            email=form.email.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Registration', form=form)
 
 
 def main():
